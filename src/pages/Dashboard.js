864b21e24e1998235a43bac23b2527727f21294a
Jomana Ahmed <ojy.ahmed@outlook.com>
2021-08-21 01:48:44 +0200

    Displaying the Countries data on the browser & the Console

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,34 @@
-import getData from './getData'
+import { useState, useEffect } from 'react'
+
 
 function Dashboard() {
-    return(
-      <>ddddddd</>
-    ) 
+  const [countriesData, setCountriesData] = useState(null)
+  console.log('countriesData', countriesData)
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('https://restcountries.eu/rest/v2/all')
+      const data = await response.json()
+
+      setCountriesData(data)
+    }
+    getData()
+  }, [])
+
+  if(!countriesData) {
+    return <>loading</>
+  }
+
+  return (
+    <div>{countriesData.map((item) => {
+      return (
+        <>
+        <div>{item.name}</div>
+        <div>{item.region}</div>
+        </>
+      )
+    })}</div>
+  )
 }
 
 export default Dashboard
